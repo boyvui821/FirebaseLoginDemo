9e8c099a92bd17cf92c9948f04156341f130c77e
Nguyen Hieu Trung <ktpm11520669@gmail.com>
2018-09-11 21:34:43 +0700

    First Update Demo Login Facebook and Google

--- FirebaseLoginDemo/AppDelegate.swift
+++ FirebaseLoginDemo/AppDelegate.swift
@@ -7,16 +7,63 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
+import GoogleSignIn
+import KRProgressHUD
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
-
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //Firebase Configure
+        FirebaseApp.configure();
+        
+        //Facebook
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions);
+        
+        //Google
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID;
+        GIDSignIn.sharedInstance().delegate = self
+        
         return true
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        var handled = false;
+        if (url.scheme == "fb\(FBSDKSettings.appID())"){
+            handled = FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+        }else{
+            handled = GIDSignIn.sharedInstance().handle(url, sourceApplication: sourceApplication, annotation: annotation);
+        }
+        
+        return handled;
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil{
+            print("Có lỗi xảy ra khi đăng nhập với Google")
+        }
+        
+        var authentication = user.authentication;
+        var credential = GoogleAuthProvider.credential(withIDToken: (authentication?.idToken)!, accessToken: (authentication?.accessToken)!);
+        
+        KRProgressHUD.show();
+        Auth.auth().signIn(with: credential) { (user, err) in
+            KRProgressHUD.dismiss();
+            if err != nil{
+                print(error)
+                return
+            }
+            
+            print(user);
+        }
+        
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- FirebaseLoginDemo/ViewController.swift
+++ FirebaseLoginDemo/ViewController.swift
@@ -7,18 +7,58 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FBSDKCoreKit
+import FBSDKLoginKit
+import KRProgressHUD
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func PressFBlogin(_ sender: Any) {
+        FBSDKLoginManager().logIn(withReadPermissions: ["public_profile"], from: self) { (result, error) in
+            if error != nil{
+                print("Đã có lỗi xảy ra \(error)")
+                return
+            }
+            
+            KRProgressHUD.show();
+            KRProgressHUD.show(withMessage: "Loading...")
+            let accesstoken = FBSDKAccessToken.current()
+            let strAccessToken = accesstoken?.tokenString;
+            let credential = FacebookAuthProvider.credential(withAccessToken: strAccessToken!);
+            
+            
+            Auth.auth().signIn(with: credential, completion: { (user, err) in
+                KRProgressHUD.dismiss()
+                
+                if err != nil {
+                    print("Lỗi đăng nhập FB với firebase");
+                    return
+                }
+                
+                
+                print("Đăng nhập thành công với: \(user)");
+            })
+        }
     }
+    
+    @IBAction func PressGGlogin(_ sender: Any) {
+        KRProgressHUD.show();
+        GIDSignIn.sharedInstance().signIn();
+    }
+    
+    func sign(inWillDispatch signIn: GIDSignIn!, error: Error!) {
+        KRProgressHUD.dismiss();
+    }
+    
 
 
 }
